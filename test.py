75e9609b6ed3e0a223903a7aa0ea3deee22b44ca
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-12-03 23:10:48 -0500

    Dec 3: Testing append mode CREATE option considered

--- test.py
+++ test.py
@@ -1,18 +1,46 @@
 """USED FOR TESTING - TO BE DELETED"""
 
-inv=open("inventory.csv","r")
+iRead=open("inventory.csv","r")
+inv=open("inventory.csv","a")
 
+#Changing value types in values and putting it into variable
 counter=0
-for aLine in inv:
-   values=aLine.split(",") # now a list called values
-   print(values)
-   
-   if (counter>=1):
-    values[2]=bool(values[2]) #turning Edible into True/False
-    values[3]=float(values[3]) #turning retail price to float
+var_inv=[]
+for aLine in iRead:
+    values=aLine.split(",") # now a list called values
+    print(values)
+    try:
+        if (counter>=1):
+            values[2]=bool(values[2]) #turning Edible into True/False
+            values[3]=float(values[3]) #turning retail price to float
+            var_inv.append(values) #will only have lists of the info, not titles
 
-   counter+=1
+    except Exception as e:
+        print(e)
+        continue
 
-   print(values,"\n -------------*********------")
+    finally: 
+        counter+=1
+    print(values,"\n -------------*********------")
+
+# print(var_inv)
+
+#testing how append works
+# #this is how you'd do CREATE menu, but add parameters for each section (i.e. give me name, then code, etc.)
+# user=input("Write something: ")
+# inv.write("\n"+user) #.write() needs file and requires string parameter; also add \n in front for next line
+# #inv.write(str(var_inv)) #you don't need when doing append or it GETS FUNKY (like starts adding and adding in on itself)
+
+
+
 
 inv.close()
+iRead.close()
+
+
+#ORIGINAL DATA of inventory.csv
+# Item,Specimen-Name,Edible,Retail-Price,Scientist-Name
+# Orange,ORA-123,True,3.45,Aud
+# Moss,MOS-346,False,123.26,Aud
+# "Charlie",CHA-731,True,92.6,Aud
+# Donut,DON-167,False,7000.07,Aud
